9cc13d95a662b966388df2a6561f2db7e6e1cf2b
vanil-1 <danilayanigin@gmail.com>
2026-04-07 06:53:27 +0300

    lab01: remake demo.py and README

--- src/lab01/demo.py
+++ src/lab01/demo.py
@@ -5,56 +5,101 @@ sys.path.append(str(Path(__file__).resolve().parent.parent))
 from model import House
 
 
-def demo():
-    print("\n=== 1. Создание корректного объекта ===")
-    house1 = House(
-        "12 baker street",
-        3,
-        120,
-        1000,
-        6,
-        False,
+def normal():
+    house = House(
+        address="221 baker",
+        floors=2,
+        area=120,
+        cost=1000,
+        min_time_rent=6,
+        rented=False,
     )
-    print(house1)
-    print(repr(house1))
 
-    print("\n=== 2. Проверка setter cost ===")
-    house1.cost = 1200
-    print("New cost:", house1.cost)
+    print(house)
+    print(house.cost_rent_time())
 
-    print("\n=== 3. Попытка изменить цену при аренде ===")
-    house1.make_contract()
+    print(house.make_contract())
+    print("Rented status:", house.rented)
+
+    return 0
+
+
+def warnings_errors():
+    house = House(
+        address="13 elm",
+        floors=1,
+        area=80,
+        cost=800,
+        min_time_rent=3,
+        rented=False,
+    )
+
     try:
-        house1.cost = 1500
+        house.rented = False
     except Warning as w:
         print("Warning:", w)
 
-    print("\n=== 4. Бизнес-методы ===")
-    print(house1.cost_rent_time())
+    house.make_contract()
 
-    print("\n=== 5. Проверка rented setter ===")
     try:
-        house1.rented = True
+        house.cost = 900
     except Warning as w:
         print("Warning:", w)
 
-    print("\n=== 6. Сравнение домов (__eq__) ===")
-    house2 = House(
-        "5 elm ", 2, 120, 1200, 6, False
-    )
-    print(house1 == house2)
+    try:
+        house.min_time_rent = 6
+    except Warning as w:
+        print("Warning:", w)
 
-    print("\n=== 7. Проверка валидации ===")
     try:
         bad_house = House(
-            "BAD ADDRESS", 1, 50, 100, 0, "yes"
+            address="wrong address format!!!",
+            floors=10,
+            area=5,
+            cost=50,
+            min_time_rent=0,
+            rented="yes",
         )
     except Exception as e:
         print("Validation error:", e)
 
-    print("\n=== 8. Таблица дома ===")
-    print(house2)
+    return 0
+
+
+def comparing():
+    house1 = House(
+        address="10 green",
+        floors=2,
+        area=100,
+        cost=1000,
+        min_time_rent=6,
+        rented=False,
+    )
+
+    house2 = House(
+        address="20 blue",
+        floors=3,
+        area=100,
+        cost=1200,
+        min_time_rent=6,
+        rented=False,
+    )
+
+    print(house1 == house2)
+
+
+def main():
+    scen = int(input())
+
+    if scen == 1:
+        normal()
+    elif scen == 2:
+        warnings_errors()
+    else:
+        comparing()
+
+    return 0
 
 
 if __name__ == "__main__":
-    demo()
+    main()
